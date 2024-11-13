e5215eb813fe1b9b453753c54a723224f58314d1
Anderson1605R <114451515+Anderson1605R@users.noreply.github.com>
2024-11-13 14:17:16 -0300

    ajuste da coluna short e longdescription para receber text no bd

--- dslist/src/main/java/br/com/rochadev/dslist/entites/Games.java
+++ dslist/src/main/java/br/com/rochadev/dslist/entites/Games.java
@@ -16,7 +16,9 @@ public class Games {
     private String platforms;
     private double score;
     private String imgUrl;
+    @Column(columnDefinition = "TEXT")
     private String shortDescription;
+    @Column(columnDefinition = "TEXT")
     private String longDescription;
 
     public Games() {
